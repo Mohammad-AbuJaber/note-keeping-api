0defcd56d2375237ad87f9d8ce78d0b5a13bf80a
Mohammad-AbuJaber <mohammadabujaber2001@gmail.com>
2023-09-27 15:39:59 +0300

    adding get method

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/note-keeping', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useNewUrlParser: true, useUnifiedTopology: true,
 });
 
+app.get('/notes', async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.json(notes);
+    } catch (error) {
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
